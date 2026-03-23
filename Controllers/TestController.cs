11ba746601c7e6aa506fe1234e0fb31435f3ebc6
Chaima <Shayma.trabelsi53@gmail.com>
2026-03-23 21:36:47 +0100

    initial commit SmartParking

---
+++ Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SmartParking.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [Authorize]
+        [HttpGet("secure")]
+        public IActionResult TestSecure()
+        {
+            return Ok("JWT fonctionne 🔥");
+        }
+    }
+}
